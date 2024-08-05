70d69523d2fd4c02ab2f6ce63efb0123d8118b55
yuniavitaa <yuniiswandari334@gmail.com>
2024-08-05 17:02:00 +0700

    Added Assets, frontend template, and Home

---
+++ app/Views/frontend_beranda.php
@@ -0,0 +1,146 @@
+<?= $this->extend('layout/frontend_template') ?>
+
+<?= $this->section('style') ?>
+<style>
+    .hero-section {
+        position: relative;
+        width: 100%;
+        height: 100vh;
+        background-image: url('assets/img/Background.png');
+        /* Ganti dengan path gambar Anda */
+        background-size: cover;
+        background-position: center;
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+    }
+
+    .hero-content {
+        z-index: 2;
+    }
+
+    .hero-overlay {
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+        /* Overlay gelap dengan transparansi */
+        z-index: 1;
+    }
+
+    .hero-content h1 {
+        font-size: 3rem;
+        margin-bottom: 1rem;
+    }
+
+    .hero-content p {
+        font-size: 1.5rem;
+        margin-bottom: 2rem;
+    }
+
+    .hero-content .btn {
+        background-color: transparent;
+        border: 1px solid white;
+        color: white;
+        padding: 0.75rem 2rem;
+        text-transform: uppercase;
+    }
+</style>
+<?= $this->endSection() ?>
+
+<?= $this->section('content') ?>
+
+
+<!-- Hero Section -->
+<header class="hero-section">
+    <div class="hero-overlay"></div>
+    <div class="hero-content">
+        <h1>Connecting People with Internet</h1>
+        <p>Empowering connectivity for tomorrow.</p>
+        <a href="#" class="btn">Selengkapnya</a>
+    </div>
+</header>
+
+<!-- Content for the home page -->
+<div class="container text-center pt-5">
+    <h2>Our Partner</h2>
+    <div class="row">
+        <div class="col-md-20 pt-5">
+            <img src="assets/img/Group 2.png" alt="Partner 1" class="img-fluid">
+        </div>
+    </div>
+    <div class="container text-center pt-5">
+        <h2>About Us</h2>
+        <p>PT SIMS facilitates digital living through access infrastructure, telecommunications networks,
+            and information technology services.</p>
+            <!-- Content for the home page -->
+        <div class="row mt-4">
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="assets/img/jaringan.png" class="card-img-top" alt="Partner 1">
+                    <div class="card-body equal-height">
+                        <h5 class="card-title">Konektivitas Terluas</h5>
+                        <p class="card-text">Jaringan yang luas dan andal, menjangkau berbagai daerah dengan layanan berkualitas tinggi.</p>
+                        <a href="https://partner1.com" class="btn btn-primary" target="_blank">Visit Partner 1</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="assets/img/teknologi.png" class="card-img-top" alt="Partner 2">
+                    <div class="card-body equal-height">
+                        <h5 class="card-title">Teknologi Terkini</h5>
+                        <p class="card-text">Teknologi mutakhir untuk memastikan kecepatan, stabilitas, dan efisiensi dalam setiap layanan.</p>
+                        <a href="https://partner2.com" class="btn btn-primary" target="_blank">Visit Partner 2</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="assets/img/support.png" class="card-img-top" alt="Partner 3">
+                    <div class="card-body equal-height">
+                        <h5 class="card-title">Dukungan Terbaik</h5>
+                        <p class="card-text">Layanan pelanggan siap membantu kapan saja, memastikan pengalaman pengguna yang memuaskan dan tanpa hambatan.</p>
+                        <a href="https://partner3.com" class="btn btn-primary" target="_blank">Visit Partner 3</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card">
+                    <img src="assets/img/inovasi.png" class="card-img-top" alt="Partner 4">
+                    <div class="card-body equal-height">
+                        <h5 class="card-title">Inovasi Berkelanjutan</h5>
+                        <p class="card-text">Revolusi teknologi dengan inovasi berkelanjutan memastikan Anda selalu berada di garis depan perkembangan digital.</p>
+                        <a href="https://partner4.com" class="btn btn-primary" target="_blank">Visit Partner 4</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <h1 style="font-weight: 900; margin-top: 100px; margin-bottom: 50px;">Carousel with text in slide</h1>
+            <div id="slider" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="item active">
+                        <div class="row">
+                            <div class="col-sm-4">
+                                <div>
+                                    <h3>What is lorem isum</h3>
+                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                                </div>
+                            </div>
+                            <div class="col-sm-8">
+                                <img src="assets/img/Background.png" alt="Slide 1">
+                            </div>
+                        </div>
+                    </div>
+       
+                </div>
+            </div>
+        </div>
+        <?= $this->endSection() ?>
---
+++ app/Views/layout/frontend_template.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta http-equiv="x-ua-compatible" content="ie=edge" />
+  <title>PT SIMS</title>
+  <!-- MDB icon -->
+  <link rel="icon" href="img/SIMS.png" type="image/x-icon" />
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" />
+  <!-- Google Fonts Roboto -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" />
+  <!-- MDB -->
+  <link rel="stylesheet" href="assets/css/mdb.min.css" />
+
+  <?= $this->renderSection('style') ?>
+
+
+</head>
+
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+      <a class="navbar-brand" href="#">
+        <img src="assets/img/SIMS.png" height="30" alt="SIMS Logo" loading="lazy" />
+      </a>
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item"><a class="nav-link" href="#">Products</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Experiences</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">About Us</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Contact Us</a></li>
+          <li class="nav-item"><a class="nav-link" href="#">Blog</a></li>
+        </ul>
+      </div>
+    </div>
+    </div>
+  </nav>
+  <!-- Navbar -->
+
+  <!-- Main content -->
+  <?= $this->renderSection('content') ?>
+
+  <!-- Footer -->
+  <footer class="text-center text-lg-start bg-body-tertiary text-muted">
+    <!-- Section: Social media -->
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+      <!-- Left -->
+      <div class="me-5 d-none d-lg-block">
+        <span>Get connected with us on social networks:</span>
+      </div>
+      <!-- Left -->
+
+      <!-- Right -->
+      <div>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-twitter"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-google"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-instagram"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-linkedin"></i>
+        </a>
+        <a href="" class="me-4 text-reset">
+          <i class="fab fa-github"></i>
+        </a>
+      </div>
+      <!-- Right -->
+    </section>
+    <!-- Section: Social media -->
+
+    <!-- Section: Links  -->
+    <section class="">
+      <div class="container text-center text-md-start mt-5">
+        <!-- Grid row -->
+        <div class="row mt-3">
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+            <!-- Content -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              <i class="fas fa-gem me-3"></i>Company name
+            </h6>
+            <p>
+              Here you can use rows and columns to organize your footer content. Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit.
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Products
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Angular</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">React</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Vue</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Laravel</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">
+              Useful links
+            </h6>
+            <p>
+              <a href="#!" class="text-reset">Pricing</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Settings</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Orders</a>
+            </p>
+            <p>
+              <a href="#!" class="text-reset">Help</a>
+            </p>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+            <!-- Links -->
+            <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+            <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>
+            <p>
+              <i class="fas fa-envelope me-3"></i>
+              info@example.com
+            </p>
+            <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>
+            <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>
+          </div>
+          <!-- Grid column -->
+        </div>
+        <!-- Grid row -->
+      </div>
+    </section>
+    <!-- Section: Links  -->
+
+    <!-- Copyright -->
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+      © 2021 Copyright:
+      <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+
+
+  <!-- MDB -->
+  <script type="text/javascript" src="assets/js/mdb.umd.min.js"></script>
+  <?= $this->renderSection('script') ?>
+  <!-- Custom scripts -->
+  <script type="text/javascript"></script>
+</body>
+
+</html>
