cf15a30f2062c41f6e6869afc368922501683ae6
Cindy Intania <@cindyintania@students.amikom.ac.id>
2024-09-02 16:52:46 +0700

    Modified frontend experience

--- app/Views/frontend_experience.php
+++ app/Views/frontend_experience.php
@@ -71,21 +71,13 @@
         margin: 20px auto;
         /* Jarak antara garis dan elemen lainnya */
     }
-   .government-section {
-    display: flex;
-    align-items: stretch;
-    min-height: 300px; /* Sesuaikan tinggi minimum sesuai kebutuhan */
-}
 
-.government-image {
-    width: 100%;
-    height: 100%;
-    object-fit: cover; /* Gambar memenuhi kolom tanpa distorsi */
-}
-
-.col-md-6, .col-sm-8, .col-sm-4 {
-    display: flex;
-}
+    .government-image {
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+        /* Gambar memenuhi kolom tanpa distorsi */
+    }
 </style>
 <?= $this->endSection() ?>
 
@@ -103,11 +95,11 @@
 </div>
 <!-- Gevorment -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-md-6 d-flex align-items-center justify-content-center government-text">
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 d-flex align-items-center justify-content-center text-center ">
             <div>
                 <h4 class="text-experience mb-4 arcade-interlaced-semibold">GOVERNMENT</h4>
-                <p class="chakra-petch-regular">It has experiences in telecommunications network, dedicated internet FO, VPN, SKPD Network, District/Village Internet, Training Assistance, Smart Government Consultation.</p>
+                <p class="chakra-petch-regular">It has experiences in <br>telecommunications network,<br> dedicated internet FO, VPN, SKPD <br> Network, District/Village Internet,<br> Training Assistance, Smart <br> Government Consultation.</p>
             </div>
         </div>
         <div class="col-md-6 p-0">
@@ -115,6 +107,7 @@
         </div>
     </div>
 </div>
+<br>
 <br>
 
 <!-- Logo Government -->
@@ -132,15 +125,15 @@
 
 <!-- Konten Sarjito -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-sm-8 p-0">
-            <img src="assets/img/exsarjito.png" alt="Slide 1" class="img-fluid government-image">
-        </div>
-        <div class="col-sm-4 d-flex align-items-center justify-content-center government-text">
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 p-0  ">
             <div>
-                <h4 class="text-experience mb-4 arcade-interlaced-semibold">BUSINESS/COMMERCIAL</h4>
-                <p class="chakra-petch-regular">Dedicated Internet, Broadband Internet, SOHO, VPN.</p>
+                <img src="assets/img/exsarjito.png" alt="Government Image" class="img-fluid government-image">
             </div>
+        </div>
+        <div class="col-md-6 align-items-center justify-content-center text-center">
+        <h4 class="text-experience mb-4 arcade-interlaced-semibold">BUSINESS/ <br> COMMERCIAL</h4>
+        <p class="chakra-petch-regular">Dedicated Internet, Broadband Internet, SOHO, VPN.</p>
         </div>
     </div>
 </div>
@@ -160,18 +153,17 @@
 <br>
 <!-- Konten ISP -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-md-6 government-text">
-            <div class="d-flex align-items-center h-50 ">
-                <div>
-                    <h4 class="text-experience mb-4 arcade-interlaced-semibold">ISP/TELCO OPERATORS</h4>
-                    <P class="chakra-petch-regular">Dark fiber, BTS-hotel, microcell 
-                        <br> backbone</P>
-                </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 d-flex align-items-center justify-content-center text-center ">
+            <div>
+                <h4 class="text-experience mb-4 arcade-interlaced-semibold">ISP/TELCO<br>OPERATORS</h4>
+                <P class="chakra-petch-regular">Dark fiber, BTS-hotel, microcell
+                    <br> backbone
+                </P>
             </div>
         </div>
-        <div class="col-md-6">
-            <img src="assets/img/exisp.png" style="object-fit: cover;" >
+        <div class="col-md-6 p-0">
+            <img src="assets/img/exisp.png" alt="Government Image" class="img-fluid government-image">
         </div>
     </div>
 </div>
@@ -192,18 +184,18 @@
 
 <!-- Konten Hotel -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-sm-8">
-            <img src="assets/img/exhotel.png" alt="Slide 1" class="d-block w-100">
-        </div>
-        <div class="col-sm-4">
-            <div class="d-flex align-items-center h-100 ">
-                <div>
-                    <h4 class="text-experience mb-4 arcade-interlaced-semibold">HOTEL</h4>
-                    <p class="chakra-petch-regular">Pay TV, dedicated internet, WiFi, 
-                        <br>hotspot access, MATV</p>
-                </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 p-0">
+            <div>
+                <img src="assets/img/exhotel.png" alt="Government Image" class="img-fluid government-image">
             </div>
+        </div>
+        <div class="col-md-6 align-items-center justify-content-center text-center">
+            <h4 class="text-experience mb-4 arcade-interlaced-semibold">HOTEL</h4>
+            <p class="chakra-petch-regular">Pay TV, dedicated internet, WiFi,
+                <br>hotspot access, MATV
+            </p>
+
         </div>
     </div>
 </div>
@@ -223,17 +215,15 @@
 
 <!-- Konten apart -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-md-6 government-text">
-            <div class="d-flex align-items-center h-50 ">
-                <div>
-                    <h4 class="text-experience mb-4 arcade-interlaced-semibold">APARTMENT/REAL ESTATE</h4>
-                    <P class="chakra-petch-regular">It has experiences in <br>telecommunications network, <br>dedicated internet FO, VPN, SKPD <br> Network, District/Village Internet,<br> Training Assistance, Smart <br> Government Consultation.</P>
-                </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 d-flex align-items-center justify-content-center text-center ">
+            <div>
+                <h4 class="text-experience mb-4 arcade-interlaced-semibold">APARTMENT/ <br>REAL ESTATE</h4>
+                <P class="chakra-petch-regular">It has experiences in <br>telecommunications network, <br>dedicated internet FO, VPN, SKPD <br> Network, District/Village Internet,<br> Training Assistance, Smart <br> Government Consultation.</P>
             </div>
         </div>
-        <div class="col-md-6">
-            <img src="assets/img/exaprt.png" style="object-fit: cover;">
+        <div class="col-md-6 p-0">
+            <img src="assets/img/exaprt.png" alt="Government Image" class="img-fluid government-image">
         </div>
     </div>
 </div>
@@ -253,17 +243,16 @@
 
 <!-- Konten Education -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-sm-8">
-            <img src="assets/img/exedu.png" alt="Slide 1" class="d-block w-100">
-        </div>
-        <div class="col-sm-4">
-            <div class="d-flex align-items-center h-100 ">
-                <div>
-                    <h4 class="text-experience mb-4 arcade-interlaced-semibold">EDUCATION</h4>
-                    <p class="chakra-petch-regular">Dedicated Internet, dark fiber,<br> interconnection, internship.</p>
-                </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 p-0">
+            <div>
+                <img src="assets/img/exedu.png" alt="Government Image" class="img-fluid government-image">
             </div>
+        </div>
+        <div class="col-md-6 align-items-center justify-content-center text-center">
+            <h4 class="text-experience mb-4 arcade-interlaced-semibold">EDUCATION</h4>
+            <p class="chakra-petch-regular">Dedicated Internet, dark fiber,<br> interconnection, internship.</p>
+
         </div>
     </div>
 </div>
@@ -283,22 +272,21 @@
 
 <!-- Konten Banking -->
 <div class="container-fluid government-section">
-    <div class="row">
-        <div class="col-md-6 government-text">
-            <div class="d-flex align-items-center h-50 ">
-                <div>
-                    <h4 class="text-experience mb-4 arcade-interlaced-semibold ">BANKING</h4>
-                    <P class="chakra-petch-regular">Dedicated internet, broadband <br> internet, managed service, WiFi <br hotspot access.</P>
-                </div>
+    <div class="row mb-5 align-items-center">
+        <div class="col-md-6 d-flex align-items-center justify-content-center text-center">
+            <div>
+                <h4 class="text-experience mb-4 arcade-interlaced-semibold ">BANKING</h4>
+                <P class="chakra-petch-regular">Dedicated internet, broadband <br> internet, managed service, WiFi <br hotspot access.</P>
+
             </div>
         </div>
-        <div class="col-md-6">
-            <img src="assets/img/exbank.png" style="object-fit: cover;">
-        </div>     
+        <div class="col-md-6 p-0">
+            <img src="assets/img/exbank.png" alt="Government Image" style="object-fit: cover;" >
+        </div>
     </div>
 </div>
 <br>
-<!-- Logo APART -->
+<!-- Logo Banking -->
 <div class="container">
     <div class="row">
         <div class="col d-flex flex-column align-items-center">
