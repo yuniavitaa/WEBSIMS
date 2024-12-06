51fb3e2a4243e9d53872e8e9bf6ee1c5828047cf
yuniavitaa <yuniiswandari334@gmail.com>
2024-12-06 11:21:01 +0700

    Modified form

--- app/Controllers/PendaftaranAnggota.php
+++ app/Controllers/PendaftaranAnggota.php
@@ -3,6 +3,7 @@
 namespace App\Controllers;
 
 use App\Models\PendaftaranAnggotaModel;
+use App\Models\BuktiPembayaranModel;
 
 class PendaftaranAnggota extends BaseController
 {
@@ -63,27 +64,45 @@ class PendaftaranAnggota extends BaseController
             'gender' => $this->request->getPost('gender'),
             'payment_method' => $this->request->getPost('payment_method'),
         ];
-
         $model = new PendaftaranAnggotaModel();
         if ($model->save($data)) {
-            // Tambah notifikasi
+            $lastInsertId = $model->insertID(); // Ambil ID terakhir yang disimpan
+
+            // Simpan ID pendaftaran ke dalam session
+            session()->set('pendaftaran_id', $lastInsertId);
+
+            // Simpan notifikasi
             $notifikasi = session()->get('notifikasi') ?? 0;
             session()->set('notifikasi', $notifikasi + 1);
 
-            return redirect()->back()->with('sukses', 'Data berhasil dikirim!');
+            // Arahkan ke halaman upload bukti pembayaran
+            return redirect()->to('/pendaftaran/uploadBukti/' . $lastInsertId)
+                ->with('sukses', 'Data berhasil dikirim! Silakan unggah bukti pembayaran.');
         } else {
             return redirect()->back()->withInput()->with('gagal', 'Terjadi kesalahan saat menyimpan data.');
         }
     }
 
+    // Menampilkan halaman upload bukti pembayaran
+    public function uploadBukti($pendaftaran_id)
+    {
+        // Ambil data pendaftaran berdasarkan ID
+        $model = new \App\Models\PendaftaranAnggotaModel();
+        $dataPendaftaran = $model->find($pendaftaran_id);
+
+        if (!$dataPendaftaran) {
+            return redirect()->to('/pendaftaran')->with('gagal', 'Data pendaftaran tidak ditemukan.');
+        }
+
+        // Tampilkan halaman upload bukti pembayaran
+        return view('upload_bukti_pembayaran', ['dataPendaftaran' => $dataPendaftaran]);
+    }
+
+    // Proses upload bukti pembayaran
     public function prosesUploadBukti()
     {
         $validation = $this->validate([
             'pendaftaran_id' => 'required|integer',
-            'nama' => 'required',
-            'email' => 'required|valid_email',
-            'nomor_hp' => 'required|numeric',
-            'payment_method' => 'required|in_list[transfer,gopay,shoppepay,dana]',
             'bukti_pembayaran' => 'uploaded[bukti_pembayaran]|max_size[bukti_pembayaran,2048]|is_image[bukti_pembayaran]|mime_in[bukti_pembayaran,image/png,image/jpg,image/jpeg]',
         ]);
 
@@ -97,16 +116,12 @@ class PendaftaranAnggota extends BaseController
             $fileName = $file->getRandomName();
             $file->move(WRITEPATH . 'uploads', $fileName);
 
-            // Data yang akan disimpan ke database
-            $model = new \App\Models\BuktiPembayaranModel();
+            $model = new BuktiPembayaranModel();
             $model->save([
                 'pendaftaran_id' => $this->request->getPost('pendaftaran_id'),
-                'nama' => $this->request->getPost('nama'),
-                'email' => $this->request->getPost('email'),
-                'nomor_hp' => $this->request->getPost('nomor_hp'),
-                'payment_method' => $this->request->getPost('payment_method'),
                 'bukti_pembayaran' => $fileName,
             ]);
+
             session()->set('notifikasi', session()->get('notifikasi') - 1);
 
             return redirect()->back()->with('sukses', 'Bukti pembayaran berhasil diupload!');
--- app/Views/templates/admin_dashboard.php
+++ app/Views/templates/admin_dashboard.php
@@ -1,209 +1,53 @@
-
 <!DOCTYPE html>
-<!--
-This is a starter template page. Use this page to start your new project from
-scratch. This page gets rid of all links and provides the needed markup only.
--->
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title><?= $judul ?? 'AdminLTE Dashboard' ?></title>
+  <base href="<?= base_url('AdminLTE-master/') ?>/">
 
-  <title>AdminLTE 3 | Starter</title>
-  <base href="<?php echo base_url('AdminLTE-master')?>/">
-  <!-- Font Awesome Icons -->
+  <!-- CSS -->
   <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
-  <!-- Theme style -->
   <link rel="stylesheet" href="dist/css/adminlte.min.css">
-  <!-- Google Font: Source Sans Pro -->
-  <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+  <link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
+  <div class="wrapper">
 
-  <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
-    <!-- Left navbar links -->
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
-      </li>
-    </ul>
-
-    <!-- Right navbar links -->
-    <ul class="navbar-nav ml-auto">
-    <form class="form-inline ml-3">
-      <div class="input-group input-group-sm">
-        <input class="form-control form-control-navbar" type="search" placeholder="Search" aria-label="Search">
-        <div class="input-group-append">
-          <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-        </div>
-      </div>
-    </form
-    </ul>
-  </nav>
-  <!-- /.navbar -->
-
-  <!-- Main Sidebar Container -->
-  <aside class="main-sidebar sidebar-dark-primary elevation-4">
-    <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
-           style="opacity: .8">
-      <span class="brand-text font-weight-light">Dashboard Admin</span>
-    </a>
+    <!-- Navbar -->
+    <!-- <?= view('partials/navbar') ?> -->
 
     <!-- Sidebar -->
-    <div class="sidebar">
-      <!-- Sidebar user panel (optional) -->
-      <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-        <div class="image">
-          <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
-        </div>
-        <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+    <?= view('partials/sidebar', ['menu' => $menu ?? '']) ?>
+
+    <!-- Content Wrapper -->
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <h1 class="m-0"><?= $judul ?></h1>
         </div>
       </div>
-
-      <!-- Sidebar Menu -->
-      <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-               <li class="nav-item">
-            <a href="<?= base_url('dashboard')?>" class="nav-link <?= $menu=='dashboard' ? 'active' : ''?>">
-              <i class="nav-icon fas fa-tachometer-alt"></i>
-              <p>
-                Dashboard
-              </p>
-            </a>
-          </li>
-
-          <li class="nav-item <?= $menu=='masterdata' ? 'menu-open' : ''?>">
-            <a href="#" class="nav-link <?= $menu =='masterdata' ? 'active' : ''?>">
-              <i class="nav-icon fas fa-table"></i>
-              <p>
-               Master Data
-                <i class="right fas fa-angle-left"></i>
-              </p>
-            </a>
-            <ul class="nav nav-treeview">
-              <li class="nav-item">
-                <a href="<?= base_url('payment') ?>" class="nav-link <?= $submenu =='payment' ? 'active' : ''?>">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Payment</p>
-                </a>
-              </li>
-              <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Blog</p>
-                </a>
-              </li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a href="<?= base_url('user')?>" class="nav-link  <?= $menu=='user' ? 'active' : ''?>">
-              <i class="nav-icon fas fa-user"></i>
-              <p>
-               User
-               
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
-              <p>
-              Setting
-               
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
-
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark"><?= $judul ?></h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!-- Main content -->
-    <div class="content">
-      <div class="container-fluid">
-        <div class="row">
-          <!-- /.content-->
-       <?php
-       if ($page) {
-       echo view($page);
-       }
-       ?>
-          <!-- /.end content -->
+      <div class="content">
+        <div class="container-fluid">
+          <?= $this->renderSection('content') ?>
         </div>
-        <!-- /.row -->
-      </div><!-- /.container-fluid -->
+      </div>
     </div>
-    <!-- /.content -->
+
+    <!-- Footer -->
+    <?= view('partials/footer') ?>
   </div>
-  <!-- /.content-wrapper -->
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
-    <!-- Control sidebar content goes here -->
-    <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
-    </div>
-  </aside>
-  <!-- /.control-sidebar -->
-
-  <!-- Main Footer -->
-  <footer class="main-footer">
-    <!-- To the right -->
-    <div class="float-right d-none d-sm-inline">
-      Anything you want
-    </div>
-    <!-- Default to the left -->
-    <strong>Copyright &copy; 2014-2019 <a href="https://adminlte.io">AdminLTE.io</a>.</strong> All rights reserved.
-  </footer>
-</div>
-<!-- ./wrapper -->
-
-<!-- REQUIRED SCRIPTS -->
-
-<!-- jQuery -->
-<script src="plugins/jquery/jquery.min.js"></script>
-<!-- Bootstrap 4 -->
-<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<!-- AdminLTE App -->
-<script src="dist/js/adminlte.min.js"></script>
+  <!-- Scripts -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="dist/js/adminlte.min.js"></script>
+  <script src="plugins/datatables/jquery.dataTables.min.js"></script>
+  <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+  <script src="plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
 </body>
+
 </html>
